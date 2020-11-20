6f980b25b99fbb1485c18a8a361299c22b2cf2cc
Rajan V <vembarrajan@gmail.com>
2020-11-20 21:26:24 +0530

    Publish Changes

---
+++ src/components/button/Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="[buttonClass, 'rb-button']" @click="click()">
+    <div class="rb-button__content">
+      <span v-if="text" class="rb-button__text">{{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RbButton',
+  props: {
+    text: String,
+    size: {
+      type: String,
+      default: 'm',
+    },
+    iconLeft: {
+      type: String,
+      default: null,
+    },
+    iconRight: {
+      type: String,
+      default: null,
+    },
+    type: {
+      default: 'hollow',
+      type: String,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    clickFn: Function,
+  },
+  computed: {
+    buttonClass() {
+      return `rb-button--${this.type} rb-button__size--${this.size}`;
+    },
+  },
+  methods: {
+    click() {
+      if (this.clickFn) {
+        this.clickFn(this.$el, this.$parent);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,42 +1,18 @@
 /* eslint-disable */
-import VrSelect from "./components/VrSelect.vue";
+import Button from "./components/button";
 
 const components = {
-  VrSelect,
-}
+  Button
+};
 
 components.install = (Vue, options = {}) => {
-  // Options
-  // setOptions(Object.assign(config, options))
-
   for (const componentName in components) {
-    const component = components[componentName]
+    const component = components[componentName];
 
-    if (component && componentName !== 'install') {
-      Vue.component(component.name, component)
+    if (component && componentName !== "install") {
+      Vue.component(component.name, component);
     }
   }
-}
+};
 
-export default components
-
-// import Vue from 'vue';
-// import App from './App.vue';
-// Vue.config.productionTip = false;
-// import VueTippy, { TippyComponent } from "vue-tippy";
-// Vue.component("tippy", TippyComponent);
-
-// Vue.use(VueTippy, {
-//   directive: "tippy", // => v-tippy
-//   flipDuration: 0,
-//   popperOptions: {
-//     modifiers: {
-//       preventOverflow: {
-//         enabled: false
-//       }
-//     }
-//   }
-// });
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount('#app');
+export default components;
